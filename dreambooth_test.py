8887aa6a6faf0c00e62534fbcccf303a756bc690
11291996 <paneah@yonsei.ac.kr>
2024-02-10 17:40:09 +0900

    integrated test.py to a notebook

--- dreambooth_test.py
+++
@@ -1,6 +0,0 @@
-from diffusers import DiffusionPipeline
-
-model_path = "/data7/OnomaAi101/CAT/results/dreambooth/cat_statue_20240207_new/"  # Adjust this to your model's path
-pipeline = DiffusionPipeline.from_pretrained(model_path, use_auth_token=True).to("cuda")
-image = pipeline("cat statue, <shs>, on a green field").images[0]
-image.save("/data7/OnomaAi101/CAT/results/dreambooth/cat_statue_20240207_new/dreambooth_<shs>, on a green field_0.png")
--- test.py
+++
@@ -1,4 +0,0 @@
-import os 
-
-test = "test2"
-os.mkdir(test + "/test3")
