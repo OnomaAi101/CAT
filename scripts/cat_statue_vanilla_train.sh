fedd24ce0f46807e869f8250f6eff26509fe242d
AngelBottomless <aria1th@naver.com>
2024-03-26 10:51:52 +0900

    add_and_fix_configs

---
+++ scripts/cat_statue_vanilla_train.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Run the training script in main repo folder
+# this assumes you are already using proper conda environment / virtualenv
+python cat_train_all.py --tuning_config_path configs/vanilla_tuning_config_cat_statue.json
